a7da8a429f23d8d9cbd4692c5dd2c400d221489b
fsossai <federico.sossai@gmail.com>
2021-10-04 15:03:29 +0200

    Exceptions, Plot and Pen

--- include/askiplot/askiplot.hpp
+++ include/askiplot/askiplot.hpp
@@ -16,8 +16,12 @@
 #ifndef ASKIPLOT_HPP_
 #define ASKIPLOT_HPP_
 
+#include <sys/ioctl.h>
+#include <unistd.h>
+
 #include <iostream>
 #include <string>
+#include <vector>
 
 #define ASKIPLOT_VERSION_MAJOR 0
 #define ASKIPLOT_VERSION_MINOR 1
@@ -28,6 +32,110 @@ namespace askiplot {
 const std::string kDefaultPenLine = "=";
 const std::string kDefaultPenArea = "#";
 const std::string kDefaultPenEmpty = " ";
+
+class InvalidPlotSize : public std::exception {
+public:
+  virtual const char* what() const noexcept override {
+    return "Plot height and width must be positive integer numbers.";
+  }
+};
+
+class PenNotSet : public std::exception {
+public:
+  virtual const char* what() const noexcept override {
+    return "Trying to access a Pen pointer that has not been set.";
+  }
+};
+
+class Pen {
+public:
+  Pen() = default;
+  virtual ~Pen() = default;
+  virtual std::string GetValue() const { return value_; }
+  virtual void SetDefaultValue() = 0;
+  virtual void SetValue(const std::string&) = 0;
+protected:
+  std::string value_;
+};
+
+class ASCIIPen : public Pen {
+public:
+  ASCIIPen(const std::string& value) { SetValue(value); }
+  void SetDefaultValue() override { value_ = " "; }
+  void SetValue(const std::string& value) override {
+    if (value.size() == 0) {
+      SetDefaultValue();
+    } else {
+      value_.resize(1);
+      value_[0] = value[0];
+    }
+  }
+private:
+};
+
+class UTF16Pen : public Pen {
+public:
+  UTF16Pen(const std::string& value) { SetValue(value); }
+  void SetDefaultValue() override { value_ = "\u0020"; }
+  void SetValue(const std::string& value) override {
+    if (value.size() < 2) {
+      SetDefaultValue();
+    } else {
+      value_.resize(2);
+      value_[0] = value[0];
+      value_[1] = value[1];
+    }
+  }
+};
+
+class Plot {
+public:
+  Plot(std::string label = "", int height = 0, int width = 0) 
+      : label_(label) {
+    if (height < 0 || width < 0) {
+      throw InvalidPlotSize();
+    }
+    if (height == 0 || width == 0) {
+      struct winsize w;
+      ioctl(STDOUT_FILENO, TIOCGWINSZ, &w);
+      if (height == 0) height_ = w.ws_row;
+      if (width == 0) width_ = w.ws_col;
+    }
+    canvas_.resize(height * width);
+  }
+
+  std::string GetName() const { return name_; }
+  std::string GetLabel() const { return label_; }
+
+  Pen& GetPen() {
+    if (pen_ == nullptr) {
+      throw PenNotSet();
+    }
+    return *pen_;
+  }
+
+  const Pen& GetPen() const {
+    if (pen_ == nullptr) {
+      throw PenNotSet();
+    }
+    return *pen_;
+  }
+
+  Plot& SetName(std::string name) { name_ = name; return *this; }
+  Plot& SetLabel(std::string label) { label_ = label; return *this; }
+  Plot& SetPen(Pen *pen) { pen_ = pen; return *this; }
+  
+protected:
+  std::string name_;
+  std::string label_;
+  int height_;
+  int width_;
+  std::vector<bool> canvas_;
+  Pen *pen_ = nullptr;
+private:
+};
+
+
 
 } // namespace askiplot
 

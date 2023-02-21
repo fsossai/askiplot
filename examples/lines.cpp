50e78afb301e7d3f991f2a9e7c4f0ec654cc3d4f
fsossai <federico.sossai@gmail.com>
2023-02-20 23:56:23 -0600

    Added `lines` example

---
+++ examples/lines.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <chrono>
+#include <thread>
+#include <askiplot.hpp>
+
+using namespace std;
+using namespace std::chrono_literals;
+using namespace askiplot;
+
+int main() {
+  Plot p;
+
+  auto box = Plot(16, 5).DrawTextCentered("AskiPlot", Center);
+
+  while (true) {
+    if (rand() % 2) {
+      p.DrawLineVerticalAtCol((float)rand() / RAND_MAX);
+    } else {
+      p.DrawLineHorizontalAtRow((float)rand() / RAND_MAX);
+    }
+
+    p.Fusion()(box, Center - Offset(box.GetWidth()/2, box.GetHeight()/2)).Fuse();
+
+    cout << p.Serialize();
+    this_thread::sleep_for(100ms); 
+  }
+  
+  return 0;
+}
